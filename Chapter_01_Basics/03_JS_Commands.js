c82756a25d786d791de751b82f83aa1cefb42467
VaniSundaramurthy <vani.sundaramurthy@accenture.com>
2026-05-06 21:50:42 +0530

    Initial commit: Learning Playwright files

---
+++ Chapter_01_Basics/03_JS_Commands.js
@@ -0,0 +1,19 @@
+console.log(process.version);
+console.log(process.platform);
+console.log(process.arch);
+console.log(process.cwd());
+console.log(process.pid);
+console.log(process.uptime());
+console.log(process.memoryUsage());
+/* greet("Vani");
+function greet(name) {
+  return "Hello, " + name;
+  console.log("Hello" + name);
+} */
+
+
+/*console.log("Debug info") → Prints messages to the browser console.
+
+prompt("Enter your name") → Asks the user for input.
+
+confirm("Are you sure?") → Displays a yes/no dialog. */
---
+++ Chapter_01_Basics/04_hotcode.js
@@ -0,0 +1,13 @@
+/* function add(a,b){
+    return a+b;
+}
+
+let result;
+for(let i=0;i<10000;i++){
+    result = add(i,i);
+} */
+
+ 
+let age = 15;
+const pi = 3.14;
+var city = "Chennai";
